18b4f442ec3032926e96d2cd81d4f3de2e63516d
Torres-ssf <sergio.uft@gmail.com>
2019-07-19 15:41:46 -0300

    Finish model user with sign up and log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,19 @@ class UsersController < ApplicationController
   def create
     @user = User.new(users_params)
     if @user.save
-      # redirect_to show_path
+      log_in(@user)
+      redirect_to show_path
     else
       render 'new'
     end
   end
 
   def show
+    if logged_in?
+      @user = User.find_by(id: session[:user_id])
+    else
+      redirect_to login_path
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
 
-  get   '/profile',       to: 'users#show'
-  get   '/signup',        to: 'users#new'
-  post  '/signup',        to: 'users#create'
+  get     '/login',        to: 'sessions#new'
+  post    '/login',        to: 'sessions#create'
+  delete  '/logout',       to: 'sessions#destroy'
+
+  get     '/profile',       to: 'users#show'
+  get     '/signup',        to: 'users#new'
+  post    '/signup',        to: 'users#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
